308bf52966c77c5ae77f06c8705a98c7f0ab6461
vchique <124209251+vchique@users.noreply.github.com>
2023-02-09 12:00:30 -0500

    Update SCRIPT MAIN TALLER 1 GRUPO 6 - Chique - Sánchez - Castro.R

--- scripts/SCRIPT MAIN TALLER 1 GRUPO 6 - Chique - Sánchez - Castro.R
+++ scripts/SCRIPT MAIN TALLER 1 GRUPO 6 - Chique - Sánchez - Castro.R
@@ -4,7 +4,7 @@
 require(pacman)
 library(pacman)
 
-p_load(tidyverse,rvest,skimr,boot) 
+p_load(tidyverse,rvest,fixest,stargazer,knitr,skimr,boot,grid,gridExtra) 
 
 #Scrapping tabla 1
 set_datos_1<-"https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html"
@@ -89,23 +89,16 @@ tabla_datos_10<-set_datos_10_html%>%
 #Uniendo tablas de datos
 
 base_todo <- rbind.data.frame(tabla_datos_1, tabla_datos_2, tabla_datos_3, tabla_datos_4, tabla_datos_5, tabla_datos_6, tabla_datos_7, tabla_datos_8, tabla_datos_9, tabla_datos_10)
-as.data.frame(base_todo)
-base_nueva<-select(base_todo, sex, relab, maxEducLevel, p6870, impa, isa, ie)
 
-base_nueva<-na.omit(base_nueva)
+# Cálculo del salario por hora
 
-##base_todo[is.na(base_todo) | base_todo=="-Inf"] = NA
+base_todo<-base_todo %>% 
+  mutate(salario = (p6500+p6510s1)/(hoursWorkUsual*4)) 
 
-#Cálculo del salario:
-base_nueva<-base_nueva %>%
-  mutate(salario=impa+isa+ie)
-
-#Logaritmo del salario
-base_nueva<-base_nueva %>%
-  mutate(log_salario=log(salario))
+summary(base_todo$salario)
 
 #Variable mujer. Cambio de Sex a female con female=1
-base_nueva<-base_nueva %>%
+base_todo<-base_todo %>%
   mutate(female = if_else(sex == 0, 1, 0) )
 
 
@@ -114,38 +107,38 @@ base_nueva<-base_nueva %>%
 # Reestructurando la base de datos para la pregunta 3
 
 df3<-base_todo %>%
-  select(y_salary_m_hu,age)
+  select(salario,age)
 
 # Limpiando la base de datos
 
 df3 <- df3 %>%
   drop_na()
 
-# Base de datos de personas mayores de 18 annos
+# Base de datos de personas mayores de 18 annos y sin salarios cercanos a cero
 
 df3 <- df3 %>%
-  filter(age >= 18)
+  filter(age >= 18, salario >= 1)
 
 # Transformando variables
 
 df3 <- df3 %>%
-  mutate(age2 = age^2, lnw =log(y_salary_m_hu))
+  mutate(age2 = age^2, lnw =log(salario))
 
 head(df3)
 
 # Analisis descriptivo y grafico
 
-summary(df3$y_salary_m_hu)
+summary(df3$salario)
 
 stargazer(df3, type = "text", digits = 1)
 
-stargazer(df3[c("y_salary_m_hu", "age")], header = FALSE, type = "text", 
+stargazer(df3[c("salario", "age")], header = FALSE, type = "text", 
           title = "Estadisticas Descriptivas", digits = 1,
           covariate.labels = c("Salario", "Edad"),
           summary.stat = c("n", "min", "p25", "median", "mean","p75", "sd", "max")
 )
 
-ggplot(data = df3, mapping = aes(x = age, y= lnw))+
+g1 <- ggplot(data = df3, mapping = aes(x = age, y= lnw))+
   geom_point(col = "#A80000" , size = 0.9)+
   labs(x = "edad",
        y = "logaritmo del salario por hora",
@@ -153,7 +146,9 @@ ggplot(data = df3, mapping = aes(x = age, y= lnw))+
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(data = df3)+
+g1
+
+g2<- ggplot(data = df3)+
   geom_histogram(mapping = aes(x=lnw),col= "#A80000", fill = "#A80000")+
   labs(x="logaritmo del salario",
        y="frecuencia",
@@ -161,38 +156,66 @@ ggplot(data = df3)+
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(data = df3)+
-  geom_boxplot(mapping = aes(x=y_salary_m_hu), fill = "#A80000", alpha =0.5)+
+g2
+
+g3 <- ggplot(data = df3)+
+  geom_boxplot(mapping = aes(x=salario), fill = "#A80000", alpha =0.5)+
   labs(x="salario",
        title = "Box Plot")+
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(data = df3)+
+g3
+
+g4 <- ggplot(data = df3)+
   geom_boxplot(mapping = aes(x=lnw), fill = "#A80000", alpha =0.5)+
   labs(x="logaritmo del salario",
        title = "Box Plot")+
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5))
 
-quantile(x=df3$y_salary_m_hu)
+g4
 
-IQR(x=df3$y_salary_m_hu)
+grid.arrange(g3,g4, nrow = 1, widths = c(1,1.5))
+
+quantile(x=df3$salario)
+
+IQR(x=df3$salario)
 
 # i) Regresion 
 
 reglnw <- lm(lnw ~ age + age2, data = df3)
+reglnw
 
 stargazer(reglnw, type = "text",
           title = "Resultados de la Regresion",
-          aling = TRUE,
+          aling = TRUE, digits=7,
           dep.var.labels = "Logaritmo del Salario",
-          covariate.labels = c("edad", "edad cuadrado")
+          covariate.labels = c("edad", "edad al cuadrado")
 )
+
+
+df3$res_lnw <- reglnw$residuals
 
 df3$lnw_hat = predict(reglnw)
 
-### plot of the estimated age-earning profile
+# iii) Model's in sample fit
+
+mean(df3$res_lnw)
+
+ggplot(data=df3, mapping = aes(x = lnw_hat, y = res_lnw))+
+  geom_point(col = "#A80000" , size = 0.9)+
+  geom_hline(yintercept=0, linetype="dashed")+
+  geom_smooth()+
+  labs(x = "Fitted values",
+       y = "Residuals",
+       title = "Residual Plot for Quadratic Fit")+
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5))
+
+qqnorm(df3$res_lnw)
+
+# iv) plot of the estimated age-earning profile
 
 summ = df3 %>%
   group_by(
@@ -223,7 +246,6 @@ ggplot(summ) +
 ### Bootstrap de "peak age"
 
 reglnw <- lm(lnw ~ age + age2, data = df3)
-stargazer(reglnw,type = "text")
 
 # i) Obtenemos los coeficientes de la regresion
 
@@ -255,7 +277,9 @@ eta_reglnw_fn(df3,1:nrow(df3))
 
 # Implementamos boot
 
-resultados <- boot(df3, eta_reglnw_fn, R=1000)
+set.seed(123)
+
+resultados <- boot(df3, eta_reglnw_fn, R=2000)
 resultados
 
 # Intervalo de confianza
@@ -265,73 +289,3 @@ boot.ci(resultados, type = c("norm", "basic"))
 names(resultados)
 hist(resultados$t)
 qqnorm(resultados$t, datax = T)
-
-#PROBLEMA 4 
-
-##a. Estimación incondicional logaritmo salario vs género
-base_nueva<-base_nueva %>%
-  filter(salario!=0.0)
-
-reg_1<-lm(log_salario ~ female, base_nueva)
-pred_reg_1<-predict(reg_1)
-
-## newdata=base_nueva
-length(pred_reg_1)
-nrow(base_nueva)
-
-stargazer(reg_1, type="text")
-
-#b.i. EQUAL PAY FOR EQUAL JOB - TEOREMA FWL
-#Variables que se van a utilizar en el modelo: log_salario, mujer, relación laboral, 
-#máximo nivel educativo alcanzado, tamaño de la empresa donde labora:
-
-#Cambio de nombre para variable p6870: total personas de empresa donde labora:
-base_nueva<-base_nueva %>% 
-  rename(tam_empresa = p6870)
-
-#Modelo lineal ln Salario con variables dependientes female y categoría de trabajo
-reg_2<-lm(log_salario ~ female+relab+maxEducLevel+tam_empresa, base_nueva)
-stargazer(reg_2, type="text")
-
-pred_reg_2<-predict(reg_2)
-length(pred_reg_2)
-
-#UTILIZANDO FWL:
-#PASO 1: Cálculo de los residuales de la regresión en los X2
-
-reg_3<-lm(log_salario~relab+maxEducLevel+tam_empresa, data=base_nueva)
-stargazer(reg_3, type="text")
-
-pred_reg_3<-predict(reg_3)
-
-resid_reg_3<-resid(reg_3)
-
-length(resid_reg_3)
-nrow(base_nueva)
-
-#PASO 2: Cálculo de los residuales de la regresión de X1 y X2:
-reg_4<-lm(female~relab+maxEducLevel+tam_empresa, base_nueva)
-stargazer(reg_4, type="text")
-resid_reg_4=resid(reg_4)
-
-length(resid_reg_4)
-
-#PASO 3: Regresión de ambos residuales
-reg_5<-lm(resid_reg_3 ~ resid_reg_4 , base_nueva)
-stargazer(reg_5, type="text")
-
-
-#COMPARACION CON LA REGRESION INICIAL
-stargazer(reg_5, reg_2, type="text")
-
-##BOOTSTRAP
-p_load("boot")
-
-sal_fn<-function(data,index) {
-  coef(lm(log_salario ~ female+relab+maxEducLevel+tam_empresa, data=base_todo, subset=index))
-}
-
-boot(base_todo, sal_fn, R = 1000)
-
-##
-

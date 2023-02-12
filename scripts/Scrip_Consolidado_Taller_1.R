4ee35b999f9d5806571d9a251ae518a02ea6eec7
Victor9104 <vi.sanchez@uniandes.edu.co>
2023-02-12 18:28:27 -0500

    Update Scrip_Consolidado_Taller_1.R

--- scripts/Scrip_Consolidado_Taller_1.R
+++ scripts/Scrip_Consolidado_Taller_1.R
@@ -106,9 +106,6 @@ base_nueva<-select(base_todo, sex, age, relab, maxEducLevel, p6870, y_salary_m_h
 
 base_nueva<-na.omit(base_nueva)
 
-
-
-
 # ---------------CONSTRUCCIÓN VARIABLE SALARIO-------------------#
 
 #Cálculo del salario:
@@ -410,4 +407,529 @@ boot.ci(resultados, type = c("norm", "basic"))
 
 hist(resultados$t)
 qqnorm(resultados$t, datax = T)
+
+##################
+##  EJERCICIO 4.##
+##################
+
+reg_1<-lm(log_salario_mensual_hora ~ female, base_nueva)
+pred_reg_1<-predict(reg_1)
+
+summary(base_nueva)
+
+stargazer(reg_1, type = "text",
+          title = " Regresión Log Salario - Female",
+          aling = TRUE,
+          dep.var.labels = "Logaritmo del Salario",
+          covariate.labels = c("female"),
+          digits=3
+)
+
+summary(reg_1)
+
+confint(reg_1)
+
+
+#---------------b.i. EQUAL PAY FOR EQUAL JOB - TEOREMA FWL--------------
+#Variables que se van a utilizar en el modelo: log_salario, mujer, relación laboral, 
+#máximo nivel educativo alcanzado, tamaño de la empresa donde labora:
+
+#Modelo lineal ln Salario con variables dependientes female y categoría de trabajo
+reg_2<-lm(log_salario_mensual_hora ~ female+relab+maxEducLevel+edad+edad_2+tam_empresa, base_nueva)
+stargazer(reg_2, type="text", digits=5)
+
+stargazer(reg_2, type = "text",
+          title = " Regresión Log Salario - todas las variables",
+          aling = TRUE,
+          dep.var.labels = "Logaritmo del Salario",
+          digits=5
+)
+
+base_nueva<-base_nueva%>%
+  mutate(pred_reg_2=predict(reg_2))
+
+
+summary(reg_2, digits=3)
+confint(reg_2)
+
+#--------------------UTILIZANDO FWL---------------------
+#PASO 1: Cálculo de los residuales de la regresión en los X2
+
+reg_3<-lm(log_salario_mensual_hora~relab+maxEducLevel+edad+edad_2+tam_empresa, data=base_nueva)
+stargazer(reg_3, type="text", digit=5)
+
+base_nueva<-base_nueva %>%
+  mutate(pred_reg_3=predict(reg_3))
+
+
+resid_reg_3<-resid(reg_3)
+
+#PASO 2: Cálculo de los residuales de la regresión de X1 y X2:
+reg_4<-lm(female~relab+maxEducLevel+edad+edad_2+tam_empresa, base_nueva)
+stargazer(reg_4, type="text", digit=5)
+resid_reg_4=resid(reg_4)
+
+
+#PASO 3: Regresión de ambos residuales
+reg_5<-lm(resid_reg_3 ~ resid_reg_4 , base_nueva)
+
+stargazer(reg_5, type = "text",
+          title = " Regresión Resid reg_3 Resid reg_4",
+          aling = TRUE,
+          dep.var.labels = "Logaritmo del Salario",
+          digits=5
+)
+
+#COMPARACION CON LA REGRESION INICIAL
+stargazer(reg_5, reg_2, type="text", digit=5)
+
+#Comparación de residuales
+sum(resid(reg_5)^2)
+sum(resid(reg_2)^2)
+
+
+
+##----------------------FWL CON BOOTSTRAP-----
+p_load("boot")
+
+##Bootstrap con la función completa
+sal_fn<-function(data,index) {
+  coefs<-coef(lm(log_salario_mensual_hora ~ female+relab+maxEducLevel+edad+edad_2+tam_empresa, data=base_nueva, subset=index))
+}
+
+boot(base_nueva, sal_fn, R = 1000)
+
+
+#FWL con bootstrap
+#Se crea la función de FWL
+fwl_bootstrap_log_salario<-function(data,index) {
+  #FWL is the regression of residuals on residuals
+  base_nueva$y_resid<-resid(lm(log_salario_mensual_hora~relab+maxEducLevel+edad+edad_2+tam_empresa, data=base_nueva, subset=index))
+  base_nueva$x_resid<-resid(lm(female~relab+maxEducLevel+edad+edad_2+tam_empresa, data=base_nueva, subset=index))
+  coef_interest<-coef(lm(y_resid~x_resid, data=base_nueva, subset=index))
+  coef_interest
+}
+
+#verifico que funciona
+lm(log_salario_mensual_hora~female+relab+maxEducLevel+edad+edad_2+tam_empresa,base_nueva)
+fwl_bootstrap_log_salario(base_nueva,1:nrow(base_nueva))
+#implemento Bootstrap
+boot(base_nueva, fwl_bootstrap_log_salario, R = 1000)
+
+
+###------------------Age-Wage profile-----------------
+base_nueva<-base_nueva%>%
+  mutate(mean_log_salario=mean(log_salario_mensual_hora))
+
+base_nueva<-base_nueva%>%
+  mutate(mean_pred_log_salario=mean(pred_reg_2))
+
+
+#Gráfico Salario estimado Mujeres Hombres
+
+base_mujer<-base_nueva%>%
+  filter(female==1)
+
+base_hombre<-base_nueva%>%
+  filter(female==0)
+
+reg_mujer<-lm(log_salario_mensual_hora ~ female+relab+maxEducLevel+edad+edad_2+tam_empresa, base_mujer)
+base_mujer<- base_mujer%>%
+  mutate(pred_mujer=predict(reg_mujer))
+
+
+reg_hombre<-lm(log_salario_mensual_hora ~ female+relab+maxEducLevel+edad+edad_2+tam_empresa, base_hombre)
+base_hombre<- base_hombre%>%
+  mutate(pred_hombre=predict(reg_hombre))%>%
+  rename(pred_mujer=pred_hombre) #para unirlos bajo la misma variable más adelante
+
+data_mh1<-base_mujer%>%
+  select(pred_mujer, female, relab, maxEducLevel, edad, edad_2, tam_empresa, log_salario_mensual_hora)
+
+data_mh2<-base_hombre%>%
+  select(pred_mujer, female, relab, maxEducLevel, edad, edad_2, tam_empresa, log_salario_mensual_hora)
+
+base_hombre_mujer<-rbind(data_mh1, data_mh2)
+
+summ_mujer_hombre = base_hombre_mujer %>%
+  group_by(
+    female, edad) %>% 
+  summarize(
+    mean_pred_mujer = mean(pred_mujer), .groups="drop"
+  ) 
+
+
+ggplot(summ_mujer_hombre, aes(x = edad, y = mean_pred_mujer, group=female)) +
+  geom_line(aes(color=female))+
+  geom_point()+
+  labs(
+    title = "Salario Predicho Hombres y Mujeres con variables de control",
+    x = "Edad",
+    y = "Salario"
+  ) +
+  theme_bw()
+
+
+## Gráfica Salario - todo el rango de Edad
+
+summ1 = base_nueva %>%
+  group_by(
+    female, edad) %>% 
+  summarize(
+    mean_y = mean(log_salario_mensual_hora),
+    yhat_reg = mean(pred_reg_2), .groups="drop"
+  ) 
+
+ggplot(summ1) + 
+  geom_point(
+    aes(x = edad, y = mean_y, color=female)) + 
+  geom_line(
+    aes(x = edad, y = yhat_reg), 
+    color = "green", size = 1.5
+  ) + 
+  labs(
+    title = "Log salario con variables de control por edad",
+    x = "Edad",
+    y = "ln Salario"
+  ) +
+  theme_bw()
+
+## Gráfica Salario - sólo personas entre 18 y 55 años
+base_edad_55<-base_nueva%>%
+  filter(edad<=55)
+
+summ1 = base_nueva %>%
+  group_by(
+    female, edad) %>% 
+  summarize(
+    mean_y = mean(log_salario_mensual_hora),
+    yhat_reg = mean(pred_reg_2), .groups="drop"
+  ) 
+
+ggplot(summ1) + 
+  geom_point(
+    aes(x = edad, y = mean_y, color=female)) + 
+  geom_line(
+    aes(x = edad, y = yhat_reg), 
+    color = "green", size = 1.5
+  ) + 
+  labs(
+    title = "Log salario con variables de control por edad",
+    x = "Edad",
+    y = "ln Salario"
+  ) +
+  theme_bw()
+
+##-----------------------Peak Age---------------------
+
+##Peak age para hombres y mujeres
+
+reg_edad<-lm(log_salario_mensual_hora ~ female+relab+maxEducLevel+edad+edad_2+tam_empresa, base_nueva)
+stargazer(reg_edad, type="text", digits=5)
+
+
+reg_normal<-lm(log_salario_mensual_hora ~ female+relab+maxEducLevel+edad+edad_2+tam_empresa, base_nueva)
+
+
+eta_mod2_fn<-function(data,index
+) {
+  
+  #coeficientes
+  coefs<-lm(log_salario_mensual_hora ~ +relab+maxEducLevel+edad+edad_2+tam_empresa, data=base_nueva, subset = index)$coefficients
+  
+  #coeficientes en escalares  
+  b5<-coefs[5]
+  b6<-coefs[6] 
+  
+  #calcular peak age
+  peak_age<-(-b5/(2*b6))
+  
+  #valor peak age
+  return(peak_age)
+}
+
+eta_mod2_fn(base_nueva,1:nrow(base_nueva)) 
+
+boot(base_nueva, eta_mod2_fn,R=1000)
+
+resultados<-boot(base_nueva, eta_mod2_fn,R=1000)
+resultados
+
+boot.ci(resultados, type = c("norm", "basic"))
+
+
+######Peak ages by gender
+
+##----Peak age para mujeres y hombres
+
+###Para Mujeres
+
+eta_mujer_fn<-function(data,index
+) {
+  
+  #coeficientes
+  coefs<-lm(log_salario_mensual_hora ~ female+relab+maxEducLevel+edad+edad_2+tam_empresa, data=base_mujer, subset = index)$coefficients
+  
+  #coeficientes en escalares  
+  b5<-coefs[5]
+  b6<-coefs[6] 
+  
+  #calcular peak age
+  peak_age<-(-b5/(2*b6))
+  
+  #valor peak age
+  return(peak_age)
+}
+
+eta_mod2_fn(base_mujer,1:nrow(base_mujer_65)) 
+
+boot(base_mujer, eta_mujer_fn,R=1000)
+
+resultados<-boot(base_mujer, eta_mod2_fn,R=1000)
+resultados
+
+###Para Hombres
+
+
+eta_hombre_fn<-function(data,index
+) {
+  
+  #coeficientes
+  coefs<-lm(log_salario_mensual_hora ~ female+relab+maxEducLevel+edad+edad_2+tam_empresa, data=base_hombre, subset = index)$coefficients
+  
+  #coeficientes en escalares  
+  b5<-coefs[5]
+  b6<-coefs[6] 
+  
+  #calcular peak age
+  peak_age<-(-b5/(2*b6))
+  
+  #valor peak age
+  return(peak_age)
+}
+
+eta_mod2_fn(base_hombre,1:nrow(base_hombre)) 
+
+boot(base_hombre, eta_hombre_fn,R=1000)
+
+resultados<-boot(base_hombre, eta_mod2_fn,R=1000)
+resultados
+
+##################
+##  EJERCICIO 5.##
+##################
+
+#-------------- Predicting earning --------------------
+
+
+# Para hacer reproducible el ejemplo
+set.seed(10101)
+
+#create ID column with a row number
+base_nueva$id <- 1:nrow(base_nueva)
+
+#usamos el 70% de la base de datos como entrenamiento y 30% como test 
+entrenamiento <- base_nueva %>% dplyr::sample_frac(0.70)
+test  <- dplyr::anti_join(base_nueva, entrenamiento, by = 'id')
+
+#especificaciones previas ////
+
+### Modelo 1
+model1<-lm(log_salario_mensual_hora ~ edad + edad_2, data=entrenamiento)
+test$model1<-predict(model1,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model1)^2))           #MSE del modelo 
+
+### Modelo 2
+model2<-lm(log_salario_mensual_hora ~ female, data=entrenamiento)
+test$model2<-predict(model2,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model2)^2))           #MSE del modelo 
+
+### Modelo 3
+model3<-lm(log_salario_mensual_hora ~ female + edad + edad_2 + relab + maxEducLevel + tam_empresa, data=entrenamiento)
+test$model3<-predict(model3,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model3)^2))           #MSE del modelo 
+
+### nuevas especificaciones ///
+
+### Modelo 4
+model4<-lm(log_salario_mensual_hora ~ female + poly(edad, 2):female + relab:female + maxEducLevel:female + tam_empresa:female, data=entrenamiento)
+test$model4<-predict(model4,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model4)^2))           #MSE del modelo 
+
+### Modelo 5
+model5<-lm(log_salario_mensual_hora ~ female + poly(edad, 2):female + poly(edad, 2):maxEducLevel +
+             relab:female + maxEducLevel:female + tam_empresa:female +maxEducLevel:tam_empresa +
+             relab:maxEducLevel + relab:tam_empresa, data=entrenamiento)
+
+test$model5<-predict(model5,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model5)^2))           #MSE del modelo 
+
+### Modelo 6
+model6<-lm(log_salario_mensual_hora ~ female + poly(edad, 2):female + poly(edad, 2):maxEducLevel +
+             poly(edad, 2):relab + poly(edad, 2):tam_empresa + relab:female +
+             maxEducLevel:female + tam_empresa:female +maxEducLevel:tam_empresa +
+             relab:maxEducLevel + relab:tam_empresa, data=entrenamiento)
+
+test$model6<-predict(model6,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model6)^2))           #MSE del modelo 
+
+### Modelo 7
+
+model7<-lm(log_salario_mensual_hora ~ female + poly(edad, 3):female + poly(edad, 3):maxEducLevel +
+             poly(edad, 3):relab + poly(edad, 3):tam_empresa + relab:female +
+             maxEducLevel:female + tam_empresa:female +maxEducLevel:tam_empresa +
+             relab:maxEducLevel + relab:tam_empresa, data=entrenamiento)
+
+test$model7<-predict(model7,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model7)^2))           #MSE del modelo 
+
+### Modelo 8
+
+model8<-lm(log_salario_mensual_hora ~ edad + sqrt(edad) + female + edad:female + 
+             sqrt(edad):female + edad:maxEducLevel + sqrt(edad):maxEducLevel +
+             edad:relab + sqrt(edad):relab + edad:tam_empresa + sqrt(edad):tam_empresa + relab +
+             tam_empresa + maxEducLevel + tam_empresa + relab:female + maxEducLevel:female +
+             tam_empresa:female +maxEducLevel:tam_empresa + relab:maxEducLevel + 
+             relab:tam_empresa, data=entrenamiento)
+
+test$model8<-predict(model8,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model8)^2))           #MSE del modelo 
+
+
+### Modelo 9
+
+model9<-lm(log_salario_mensual_hora ~ female + poly(edad, 5):female + poly(edad, 5):maxEducLevel +
+             poly(edad, 5):relab + poly(edad, 5):tam_empresa + relab:female +
+             maxEducLevel:female + tam_empresa:female +maxEducLevel:tam_empresa +
+             relab:maxEducLevel + relab:tam_empresa, data=entrenamiento)
+
+test$model9<-predict(model9,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model9)^2))           #MSE del modelo 
+
+### Modelo 10
+
+model10<-lm(log_salario_mensual_hora ~ female + poly(edad, 8):female + poly(edad, 8):maxEducLevel +
+              poly(edad, 8):relab + poly(edad, 8):tam_empresa + relab:female +
+              maxEducLevel:female + tam_empresa:female +maxEducLevel:tam_empresa +
+              relab:maxEducLevel + relab:tam_empresa, data=entrenamiento)
+
+test$model10<-predict(model10,newdata = test)                    # Predicción
+with(test,mean((log_salario_mensual_hora-model10)^2))            #MSE del modelo 
+
+
+mse1<-with(test,round(mean((log_salario_mensual_hora-model1)^2),4))
+mse2<-with(test,round(mean((log_salario_mensual_hora-model2)^2),4))
+mse3<-with(test,round(mean((log_salario_mensual_hora-model3)^2),4))
+mse4<-with(test,round(mean((log_salario_mensual_hora-model4)^2),4))
+mse5<-with(test,round(mean((log_salario_mensual_hora-model5)^2),4))
+mse6<-with(test,round(mean((log_salario_mensual_hora-model6)^2),4))
+mse7<-with(test,round(mean((log_salario_mensual_hora-model7)^2),4))
+mse8<-with(test,round(mean((log_salario_mensual_hora-model8)^2),4))
+mse9<-with(test,round(mean((log_salario_mensual_hora-model9)^2),4))
+mse10<-with(test,round(mean((log_salario_mensual_hora-model10)^2),4))
+
+mse<-c(mse1,mse2,mse3,mse4,mse5,mse6,mse7,mse8,mse9,mse10)
+
+db_mse<-data.frame(model=factor(c("model1","model2","model3","model4","model5","model6","model7",
+                                  "model8","model9","model10"),ordered=TRUE),MSE=mse)
+db_mse
+
+# Mejor modelo
+
+stargazer(model8, type = "text",
+          title = " Regresion con mejor error predictivo (model8)",
+          aling = TRUE,
+          dep.var.labels = "Logaritmo del Salario"
+)
+
+
+#PLOT DE LOS ERRORES DE PREDICCION 
+
+res_pred_modelo_8<- resid(model8,newdata = test)
+
+hist(res_pred_modelo_8, main = "Distribucion de los errores de prediccion (model8)",
+     xlab = "Error de prediccion")
+
+hist(res_pred_modelo_8, main = "Distribucion de los errores de prediccion (model8)",
+     xlab = "Error de prediccion", freq = FALSE)
+
+pred_modelo_8<-predict(model8,newdata = test)
+
+##########
+##LOOCV
+## Modelo 8
+set.seed(1010)
+folds=2000
+
+index<-split(1:2000, 1:folds)
+splt<-lapply(1:folds, function(ind) base_nueva[index[[ind]], ])
+View(head(splt[[1]]))
+
+p_load(data.table)
+
+m8<-lapply(1:folds, function(ii) lm(log_salario_mensual_hora ~ edad + sqrt(edad) + female + edad:female + 
+                                      sqrt(edad):female + edad:maxEducLevel + sqrt(edad):maxEducLevel +
+                                      edad:relab + sqrt(edad):relab + edad:tam_empresa + sqrt(edad):tam_empresa + relab +
+                                      tam_empresa + maxEducLevel + tam_empresa + relab:female + maxEducLevel:female +
+                                      tam_empresa:female +maxEducLevel:tam_empresa + relab:maxEducLevel + 
+                                      relab:tam_empresa, data = rbindlist(splt[-ii])))
+
+##Predicción en elfold que dejé por fuera
+
+p8<-lapply(1:folds, function(ii) data.frame(predict(m8[[ii]], newdata = rbindlist(splt[ii]))))
+
+for (i in 1:folds) {
+  colnames(p8[[i]])<-"yhat" 
+  splt[[i]] <- cbind(splt[[i]], p8[[i]])
+  
+}
+
+head(splt[[1]])
+
+#Cálculo MSE de cada fold
+
+MSE_k <- lapply(1:folds, function(ii) mean((splt[[ii]]$log_salario_mensual_hora - splt[[ii]]$yhat)^2))
+MSE_k
+
+#Calculo la media de los MSE
+
+mean(unlist(MSE_k))
+
+## Modelo 7
+
+set.seed(1010)
+folds=2000
+
+index<-split(1:2000, 1:folds)
+splt<-lapply(1:folds, function(ind) base_nueva[index[[ind]], ])
+View(head(splt[[1]]))
+
+p_load(data.table)
+
+m7<-lapply(1:folds, function(ii) lm(log_salario_mensual_hora ~ female + poly(edad, 3):female + poly(edad, 3):maxEducLevel +
+                                      poly(edad, 3):relab + poly(edad, 3):tam_empresa + relab:female +
+                                      maxEducLevel:female + tam_empresa:female +maxEducLevel:tam_empresa +
+                                      relab:maxEducLevel + relab:tam_empresa, data = rbindlist(splt[-ii])))
+
+##Predicción en elfold que dejé por fuera
+
+p7<-lapply(1:folds, function(ii) data.frame(predict(m7[[ii]], newdata = rbindlist(splt[ii]))))
+
+for (i in 1:folds) {
+  colnames(p7[[i]])<-"yhat" 
+  splt[[i]] <- cbind(splt[[i]], p7[[i]])
+  
+}
+
+head(splt[[1]])
+
+#Cálculo MSE de cada fold
+
+MSE_k <- lapply(1:folds, function(ii) mean((splt[[ii]]$log_salario_mensual_hora - splt[[ii]]$yhat)^2))
+MSE_k
+
+#Calculo la media de los MSE
+
+mean(unlist(MSE_k))
+
 
